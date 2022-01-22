d16a1deca73ef0d824b4d915a9af656fcd44fdc6
aditical <alishaacharya77@gmail.com>
2022-01-22 20:14:38 +0545

    under construction

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import LandingPage from './home/landingPage';
+import './assets/css/MainPage.css';
+
+function App() {
+  return (
+    <div className="mainPage">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/home/landingPage.js
@@ -0,0 +1,14 @@
+import hammer from "../assets/images/hammer.png";
+
+function LandingPage() {
+    return(
+        <div className="landingPage">
+            <div>
+                The site is under construction.....
+            </div>
+            <img src={hammer} className="hammer" alt="logo" />
+        </div>
+    )
+}
+
+export default LandingPage;

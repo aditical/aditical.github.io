b75802a6f55d47df59959bc6643567526d22f106
aditical <alishaacharya77@gmail.com>
2024-02-02 23:18:11 +0545

    feat: basic intro

--- portfolio/src/home/landingPage.jsx
+++ portfolio/src/home/landingPage.jsx
@@ -4,7 +4,12 @@ function LandingPage() {
   return (
     <div className="bg-black-200 h-screen">
       <div className="flex justify-center text-white">
-        INTRO
+        <div className="mt-16 p-16 border-white w-full">
+          Hello, I am Aditi Acharya
+          <div className="mt-2 text-6xl">
+            Software Engineer
+          </div>
+        </div>
       </div>
     </div>
   );

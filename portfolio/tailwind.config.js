b7404829497f52ff6bbf4982641eed3a6ba1292c
aditical <alishaacharya77@gmail.com>
2024-02-03 21:00:32 +0545

    feat: added icon and hello animation

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -28,6 +28,16 @@ module.exports = {
         '4xl': '2rem',
       },
       keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
         firstIcon: {
           '0%': { transform: 'translate(0px) translateY(0px)' },
           '40%': { transform: 'translateY(100px)' },
@@ -79,6 +89,7 @@ module.exports = {
         'fifth-icon': 'fifthIcon 30s infinite linear',
         'sixth-icon': 'sixthIcon 30s infinite linear',
         'seventh-icon': 'seventhIcon 30s infinite linear',
+        'waving-hand': 'wave 2s linear forwards',
       },
     },
   },
